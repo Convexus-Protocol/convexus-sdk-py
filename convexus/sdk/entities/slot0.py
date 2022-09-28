75f78461d0b99200bd0f9ed8d6cf4e7f5a0e92dc
ICONationDevTeam <iconation.team@gmail.com>
2022-09-28 18:14:44 +0200

    Add Slot0 entity

---
+++ convexus/sdk/entities/slot0.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+from typing import Dict
+
+@dataclass
+class Slot0:
+  feeProtocol: int
+  observationCardinality: int
+  observationCardinalityNext: int
+  observationIndex: int
+  sqrtPriceX96: int
+  tick: int
+  unlocked: bool
+
+  @staticmethod
+  def fromCall (data: Dict[str, str]) -> 'Slot0':
+    return Slot0 (
+      int(data['feeProtocol'], 0),
+      int(data['observationCardinality'], 0),
+      int(data['observationCardinalityNext'], 0),
+      int(data['observationIndex'], 0),
+      int(data['sqrtPriceX96'], 0),
+      int(data['tick'], 0),
+      bool(int(data['unlocked'], 0))
+    )
+
+  def __repr__(self) -> str:
+    return str(self.__dict__)
